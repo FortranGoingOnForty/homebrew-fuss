e3c5459f37e7f3555b914229bfaf84aef84d6942
espadonne <espadonne@outlook.com>
2025-11-05 09:31:19 +0000

    Update to v0.9.96

--- fuss.rb
+++ fuss.rb
@@ -1,8 +1,8 @@
 class Fuss < Formula
   desc "Tree utility for dirty git files, written in modern Fortran"
   homepage "https://github.com/FortranGoingOnForty/fuss"
-  url "https://github.com/FortranGoingOnForty/fuss/archive/refs/tags/v0.9.94.tar.gz"
-  sha256 "91e474c4ad09ad89065bac7d5463802da8d12a464608de0a90f020a9bf8e958e"
+  url "https://github.com/FortranGoingOnForty/fuss/archive/refs/tags/v0.9.96.tar.gz"
+  sha256 "ea118a108e5b47e15f74144fba937902d4198783107c309e3317957efe1412fb"
   license "MIT"
   head "https://github.com/FortranGoingOnForty/fuss.git", branch: "trunk"
 
